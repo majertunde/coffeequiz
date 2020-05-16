84e62853aadc996674a61ce7a6197db3d3278ef9
majer.t <majer.t@freemail.hu>
2020-05-16 22:24:13 +0200

    Adding buttons and sample question data layout elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coffeequiz';
+  category = 'SAMPLE';
+  id = 0;
+  airdate = new Date();
+  question = 'Sample question?';
+  answer = 'Sample answer';
+  categories : any;
 }
