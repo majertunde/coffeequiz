68097b60cf88e47835843f369762276f60c377a9
majer.t <majer.t@freemail.hu>
2020-05-16 22:28:34 +0200

    Creating timer and dummy functions for buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,40 @@ export class AppComponent {
   airdate = new Date();
   question = 'Sample question?';
   answer = 'Sample answer';
+  showAnswer = false;
+  counter = 0;
   categories : any;
+  interval : any;
+
+  setTimeOut() {
+    clearInterval(this.interval);
+    var counter = this.counter;   
+    this.interval = setInterval(() => {
+      console.log(counter);
+      if (counter == 0) {
+        this.showAnswer = true;
+      }
+
+      counter--;
+
+      if (counter < 0 ) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  getNewQuestion() {
+    console.log('getting new question...');
+    this.showAnswer = false;
+    this.setTimeOut();
+  }
+
+  raiseGoodAnswerMessage() {
+    console.log('good answer!');
+    this.getNewQuestion();
+  }
+
+  raiseWrongAnswerMessage() {
+    console.log('wrong answer!');
+  }
 }
